6e5dd379038ea2fcea5b8d99c85751b62c2bed68
Teejay012 <tijesunimioluwakoya@gmail.com>
2025-02-01 14:54:18 +0100

    fixed commit

--- src/app/contexts/constants.js
+++ src/app/contexts/constants.js
@@ -1,4 +1,4 @@
 import crowdfunding from './CrowdfundingABI.json'
 
-export const CrowdfundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const CrowdfundingAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
 export const crowdfundingABI = crowdfunding.abi;
